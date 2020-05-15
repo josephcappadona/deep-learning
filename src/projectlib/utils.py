8541fae787b351e81c1d84ee17dd8d8d65376644
Joseph Cappadona <josephcappadona27@gmail.com>
2020-05-15 11:38:58 -0400

    Added get_ordinal_encoder

--- src/projectlib/utils.py
+++ src/projectlib/utils.py
@@ -5,3 +5,11 @@ __all__ = ['format_history']
 
 def format_history(history):
     return {k: v.numpy() for k, v in history.items()}
+
+def get_ordinal_encoder(labels):
+    labels = sorted(set(labels))
+    n_unique = len(set(labels))
+    encoding = dict(list(zip(labels, list(range(n_unique)))))
+    def encoder(label):
+        return encoding[label]
+    return encoder
